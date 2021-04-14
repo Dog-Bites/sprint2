40279f73c24c0c59d498565f9ebf63b1bdd1239b
the-breanne <77354893+the-breanne@users.noreply.github.com>
2021-04-14 10:49:30 -0500

    testing a new line in settings

--- mfscrm1/settings.py
+++ mfscrm1/settings.py
@@ -121,8 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = Path(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # This is now a string
 
 
 STATICFILES_DIRS = [
